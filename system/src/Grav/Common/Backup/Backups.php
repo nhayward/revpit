879ece7a3880dd6a7a5ec41d25cf1f8747165a27
Nick Hayward <nhayward@reviseresub.com>
2026-01-26 17:27:09 -0500

    updating grav and plugins

--- system/src/Grav/Common/Backup/Backups.php
+++ system/src/Grav/Common/Backup/Backups.php
@@ -3,7 +3,7 @@
 /**
  * @package    Grav\Common\Backup
  *
- * @copyright  Copyright (c) 2015 - 2024 Trilby Media, LLC. All rights reserved.
+ * @copyright  Copyright (c) 2015 - 2025 Trilby Media, LLC. All rights reserved.
  * @license    MIT License; see LICENSE file for details.
  */
 
@@ -315,7 +315,10 @@ class Backups
      */
     protected static function convertExclude($exclude)
     {
-        $lines = preg_split("/[\s,]+/", $exclude);
+        // Split by newlines, commas, or multiple spaces
+        $lines = preg_split("/[\r\n,]+|[\s]{2,}/", $exclude);
+        // Remove empty values and trim
+        $lines = array_filter(array_map('trim', $lines));
 
         return array_map('trim', $lines, array_fill(0, count($lines), '/'));
     }
